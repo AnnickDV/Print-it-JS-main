ff112c421962edc830c96a74374fd594ee8b71a0
AnnickDV <annick.vivicorsi@gmail.com>
2024-10-31 10:47:08 +0100

    Avec commentaires

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 image:"slide4.png",
 tagLine:"Autocollants <span>avec découpe laser sur mesure</span>"
 }
-]
+] //tableau d'objet
 	
 //Ajout des bulletpoints dynamique en fonction du nombre de slides
 // appenChild est un node=un noeud. C'est un outil de navigation entre 2 éléments
@@ -25,38 +25,31 @@ dots.appendChild(document.createElement("div")).classList.add("dot");
 	}
 	
 // Fonction pour changer de slide
-let slideSelected = 0;
+let slideSelected = 0; //1ere slide du tableau
 let allDotList = document.querySelectorAll(".dot");
 allDotList[0].classList.add("dot_selected");
 function changeSlide(count){
  allDotList[slideSelected].classList.remove("dot_selected");
 slideSelected = slideSelected + count;
+//Contient l'index de l'image à afficher//Envoi selon la situation
 if(slideSelected < 0){
-slideSelected = slides.length -1;
+slideSelected = slides.length -1;//Envoi sur le dernier dot par les IF conditions
 }
 if(slideSelected >= slides.length){
-slideSelected = 0;
+slideSelected = 0;//Envoi sur le premier dot
 }
-allDotList[slideSelected].classList.add("dot_selected");
-document.querySelector(".banner-img").src = "./assets/images/slideshow/"+slides[slideSelected].image;
-document.querySelector("#banner p").innerHTML = slides[slideSelected].tagLine;
+allDotList[slideSelected].classList.add("dot_selected");//l'index contient mes slidesSelected
+document.querySelector(".banner-img").src = "./assets/images/slideshow/"+slides[slideSelected].image;//chercher image
+document.querySelector("#banner p").innerHTML = slides[slideSelected].tagLine;//réaffiche le texte
 	}
 	
-//Event du clic sur les flèches
+//Event du clic sur les flèches Si click, appel de la fonction changeSlide -1 ou +1=incrémentation compteur
 document.querySelector(".arrow_left").addEventListener("click", () =>{
 changeSlide(-1);
+console.log("fleche gauche selectionnee");
 	});
 document.querySelector(".arrow_right").addEventListener("click", () =>{
 changeSlide(1);
+console.log("fleche droite selectionnee");
 	});
 	
-//EL de l'étape 1 retirée en fin de parcours
- // event listener sur les flèches : si "click" alors action non définie sur la flèche. 
-// Le console.log permet de s'assurer que la fonction est prise en compte même vide
-// Action sur les flèches Si click sur le sélecteur arrow, alors changer de slide
-//document.querySelector(".arrow_left").addEventListener("click", () =>{
- //changeSlide(-1);
- //});
- //document.querySelector(".arrow_right").addEventListener("click", () =>{
- //changeSlide(1);
- //});
